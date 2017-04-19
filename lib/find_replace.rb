9f880790cd2583b0d504fe2b0bec3dc8828afee2
Aubrey Masten and Grady Shelton <aubrey@>
2017-04-19 13:47:02 -0700

    add code to recognize partial matches in larger words and refactor for case-insensitivity

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -1,11 +1,5 @@
 class String
   define_method(:find_replace) do |find, replace|
-    words = self.split(" ")
-    words.each_with_index do |word, index|
-      if word == find
-        words[index].replace(replace)
-      end
-    end
-    words.join(" ")
+    self.gsub(/#{find}/i, replace)
   end
 end
--- spec/find_replace_spec.rb
+++ spec/find_replace_spec.rb
@@ -10,4 +10,11 @@ describe("String#find_replace") do
     expect('hello world, my friend. hello my friend.'.find_replace('hello', 'goodbye')).to(eq('goodbye world, my friend. goodbye my friend.'))
   end
 
+  it('ignores case differences in specified word when comparing') do
+    expect('Hello World, hello my friend.'.find_replace('hello', 'goodbye')).to(eq('goodbye World, goodbye my friend.'))
+  end
+
+  it('recognizes matches of chosen word as part of larger words.') do
+    expect('my birthday was catostrophic.'.find_replace('cat', 'dog')).to(eq('my birthday was dogostrophic.'))
+  end
 end
