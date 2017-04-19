808cc510a9171eb478fbf00ff98faaf6e3f6954c
Aubrey Masten and Grady Shelton <grady@>
2017-04-19 13:25:55 -0700

    add second spec to have a given single word in a string of multiple words be targeted and replaced with another specified word. pass test

--- spec/find_replace_spec.rb
+++ spec/find_replace_spec.rb
@@ -6,4 +6,8 @@ describe("String#find_replace") do
     expect('hello'.find_replace('hello', 'goodbye')).to(eq('goodbye'))
   end
 
+  it('replaces a chosen single word within a string of words with a specified replacement') do
+    expect('hello world, my friend. hello my friend.'.find_replace('hello', 'goodbye')).to(eq('goodbye world, my friend. goodbye my friend.'))
+  end
+
 end
